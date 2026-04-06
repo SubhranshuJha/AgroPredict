ebd91ad53ef562771df076ced4161cbe44c0dc3d
JayAmbaliya81 <jayambaliya1181@gmail.com>
2026-04-06 12:53:50 +0530

    added theme context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import useTheme from './contexts/theme.jsx';
 
 function App() {
+  const { themeMode, toggleTheme } = useTheme();
 
   return (
-    <p className='text-2xl w-fit mx-auto'>Hello</p>
+
+    <div className='w-full h-screen  bg-amber-400 dark:bg-amber-950 '>
+
+      <p className='text-2xl w-fit mx-auto'>Hello</p>
+
+      <button
+        className='bg-blue-400 w-50 mx-auto p-6'
+        onClick={toggleTheme}
+
+      >
+        toggle( {themeMode} )
+      </button>
+    </div>
   )
 }
 
