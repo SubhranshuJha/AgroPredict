f4052d1896c99bf99c75a51aff09652c4fa58286
JayAmbaliya81 <jayambaliya1181@gmail.com>
2026-07-18 01:07:11 +0530

    color fixed

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -18,8 +18,15 @@ const ScrollToTop = () => {
 
 
 function Layout() {
+    const { pathname } = useLocation();
     return (
-        <div className='w-full flex flex-col min-h-screen'>
+        <div className={`w-full flex flex-col min-h-screen 
+        ${
+        pathname === "/"
+          ? "bg-[#f1f1f0] dark:bg-black"
+          : "bg-[#B4EBE6]/20 dark:bg-[#020617]"
+      }
+        `}>
 
             <Navbar />
             <ScrollToTop />
